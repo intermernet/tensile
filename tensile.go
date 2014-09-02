b682c4e31021076d22db8f62184728a2894693c8
Mike Hughes <intermernet@gmail.com>
2014-09-02 12:33:08 +1000

    Humanize size output

--- tensile.go
+++ tensile.go
@@ -207,6 +207,43 @@ func checkFlags() {
 	}
 }
 
+// ByteSize taken from http://golang.org/doc/progs/eff_bytesize.go
+type ByteSize float64
+
+const (
+	_           = iota // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func (b ByteSize) String() string {
+	switch {
+	case b >= YB:
+		return fmt.Sprintf("%.2fYB", b/YB)
+	case b >= ZB:
+		return fmt.Sprintf("%.2fZB", b/ZB)
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
+
 func main() {
 	checkFlags()
 	fmt.Printf("\n\t%s\n\n", app+version)
@@ -234,5 +271,6 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Printf("Connections:\t%d\nConcurrent:\t%d\nTotal size:\t%d bytes\nTotal time:\t%s\nAverage time:\t%s\n", conns, max, size, took, average)
+	sizeHuman := ByteSize(float64(size))
+	fmt.Printf("Connections:\t%d\nConcurrent:\t%d\nTotal size:\t%s\nTotal time:\t%s\nAverage time:\t%s\n", conns, max, sizeHuman, took, average)
 }
